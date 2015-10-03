611195fa298df4fcdbd5612fd084ebeae3bca9d3
Alena Borisenko <alena.borisenko@gmail.com>
2015-10-03 14:05:59 -0400

    Added a sprite sheet and coded walk animations into Player.prototype. In addition, separated the wasd.___.isDown if loops in order to enable diagonal movement.

--- lastcontact.js
+++ lastcontact.js
@@ -1,20 +1,25 @@
+// Game bounds
 var boundsX = 800, boundsY = 600;
 var game = new Phaser.Game(boundsX, boundsY, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update});
 
 function preload() {
-
+    // Load in the images 
     game.load.image('backdrop', 'starBG.jpg');
-    game.load.image('astronaut', 'astronaut.png');
-
-    //... enemy and such
-
+    game.load.spritesheet('astronaut', 'astro_sheet.png', 64, 64);
+    // game.load.image('enemy', 'enemy.png');
+    // game.load.image('teammate', 'teammate.png');
+    // game.load.image('button', 'button.png');
+    // Template: 
+    // game.load.image('', '.png');
 }
 
+// Object/group variables
 var player;
+// var enemy;
+// var button;
+
+// Controls
 var cursors;
-//var enemy;
-//var platform;
-//var button;
 
 function create() {
     game.physics.enable(Phaser.Physics.ARCADE);
