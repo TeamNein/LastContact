da320cff1467755930b01b8cc620956ba1d3aa20
Alena Borisenko <alena.borisenko@gmail.com>
2015-10-07 15:31:44 -0400

    Updated tilemap, fixed player sprite to have a smaller hitbox, cleaned up the code comments.

--- js/Game.js
+++ js/Game.js
@@ -1,161 +1,145 @@
 var TopDownGame = TopDownGame || {};
 
-//title screen
+// Title screen
 TopDownGame.Game = function(){};
 
 TopDownGame.Game.prototype = {
-  create: function() {
-   this.map = this.game.add.tilemap('level1');
-   var sky = this.game.add.image(0, 0, 'Background');
-    //the first parameter is the tileset name as specified in Tiled, the second is the key to the asset
-   //this.map.addTilesetImage('Background', 'Background');
-    this.map.addTilesetImage('gametilesSD', 'spacetiles');
-    
-  //   //create layer
-  //  // this.backgroundlayer = this.map.createLayer('Background');
-    this.blockedLayer = this.map.createLayer('TileLayer');
+    create: function() {
+
+        this.map = this.game.add.tilemap('level1');
+        var sky = this.game.add.image(0, 0, 'Background');
+
+        // Arguments: tileset name as specified in Tiled
+        //            key to the asset
+        this.map.addTilesetImage('tiles', 'spacetiles');
+
+        // Create layer
+        this.blockedLayer = this.map.createLayer('TileLayer');
+
+        //collision on blockedLayer
+        this.map.setCollisionBetween(0, 2000, true, 'TileLayer');
+
+        // Resize the game world to match the layer dimensions
+        this.blockedLayer.resizeWorld();
+
+        this.createKey();
+        // this.createDoors();    
+
+        // Create player
+        var result = this.findObjectsByType('playerStart', this.map, 'Objects')
+        this.player = this.game.add.sprite(result[0].x, result[0].y, 'astronaut');
+        this.player.scale.setTo(0.5, 0.5);
 
 
-  //   //collision on blockedLayer
-     this.map.setCollisionBetween(1, 200, true, 'TileLayer');
+        var spriteFPS = 5;
+        this.player.animations.add('left', [0, 1, 2], spriteFPS, true);
+        this.player.animations.add('right', [4, 5, 6], spriteFPS, true);
 
-  //   //resizes the game world to match the layer dimensions
-     this.blockedLayer.resizeWorld();
+        this.game.physics.arcade.enable(this.player);
 
-  //  // this.createItems();
-  // //  this.createDoors();    
+        //  Player physics properties
+        this.player.body.bounce.y = 0;
+        this.player.body.gravity.y = 500;
+        this.player.body.collideWorldBounds = true;
 
-  //   // //create player
-     var result = this.findObjectsByType('playerStart', this.map, 'Objects')
-     this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
+        // Makes the camera follow the player in the world
+        this.game.camera.follow(this.player);
 
-     this.game.physics.arcade.enable(this.player);
+        // Move player with cursor keys
+        this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    this.player.body.bounce.y = 0;
-    this.player.body.gravity.y = 300;
-    this.player.body.collideWorldBounds = true;
-   //  this.game.physics.arcade.enable(this.player);
+    },
+    createKey: function() {
+        // Create keys the player can pick up
+        this.keys = this.game.add.group();
+        this.keys.enableBody = true;
+        var key;    
+        result = this.findObjectsByType('key', this.map, 'Objects');
 
-  // //   // //the camera will follow the player in the world
-      this.game.camera.follow(this.player);
+        result.forEach(function(element){
+          this.createFromTiledObject(element, this.keys);
+        }, this);
+    },
+    createDoors: function() {
+        //create doors
+        this.doors = this.game.add.group();
+        this.doors.enableBody = true;
 
-  // // //   // //move player with cursor keys
-      this.cursors = this.game.input.keyboard.createCursorKeys();
+        result = this.findObjectsByType('door', this.map, 'Objects');
+        result.forEach(function(element){
+          this.createFromTiledObject(element, this.doors);
+        }, this);
+    },
 
-  },
-  createItems: function() {
-    //create items
-    this.items = this.game.add.group();
-    this.items.enableBody = true;
-    var item;    
-    result = this.findObjectsByType('item', this.map, 'objectsLayer');
-    result.forEach(function(element){
-      this.createFromTiledObject(element, this.items);
-    }, this);
-  },
-  createDoors: function() {
-    //create doors
-    this.doors = this.game.add.group();
-    this.doors.enableBody = true;
-    result = this.findObjectsByType('door', this.map, 'objectsLayer');
+    // Find objects such that containt a property called "type" equal to a certain value
+    findObjectsByType: function(type, map, layer) {
+        var result = new Array();
+        map.objects[layer].forEach(function(element){
+            if(element.properties.type === type) {
+                // Phaser uses top left, Tiled bottom left
+                // Note: keep in mind the size of objects relative to tiles
+                element.y -= map.tileHeight;
+                result.push(element);
+            }      
+        });
+        return result;
+    },
 
-    result.forEach(function(element){
-      this.createFromTiledObject(element, this.doors);
-    }, this);
-  },
+    // Create a sprite from an object
+    createFromTiledObject: function(element, group) {
+        var sprite = group.create(element.x, element.y, element.properties.sprite);
 
-  //find objects in a Tiled layer that containt a property called "type" equal to a certain value
-  findObjectsByType: function(type, map, layer) {
-    var result = new Array();
-    map.objects[layer].forEach(function(element){
-      if(element.properties.type === type) {
-        //Phaser uses top left, Tiled bottom left so we have to adjust
-        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
-        //so they might not be placed in the exact position as in Tiled
-        element.y -= map.tileHeight;
-        result.push(element);
-      }      
-    });
-    return result;
-  },
-  //create a sprite from an object
-  createFromTiledObject: function(element, gelementroup) {
-    var sprite = group.create(element.x, element.y, element.properties.sprite);
+        // Copy all properties to the sprite
+        Object.keys(element.properties).forEach(function(key){
+            sprite[key] = element.properties[key];
+        });
+    },
 
-      //copy all properties to the sprite
-      Object.keys(element.properties).forEach(function(key){
-        sprite[key] = element.properties[key];
-      });
-  },
-  update: function() {
-    //collision
-   this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    update: function() {
+        // Handle collision
+        this.game.physics.arcade.collide(this.player, this.blockedLayer);
 
-   this.player.body.velocity.x = 0;
+        this.player.body.velocity.x = 0;
 
-    if (this.cursors.left.isDown)
-    {
-        //  Move to the left
-        this.player.body.velocity.x = -150;
+        if (this.cursors.left.isDown)
+        {
+            // Move to the left
+            this.player.body.velocity.x = -150;
 
-        //player.animations.play('left');
-    }
-    else if (this.cursors.right.isDown)
-    {
-        //  Move to the right
-        this.player.body.velocity.x = 150;
+            this.player.animations.play('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            // Move to the right
+            this.player.body.velocity.x = 150;
 
-       // player.animations.play('right');
-    }
-    // else
-    // {
-    //     //  Stand still
-    //     player.animations.stop();
+            this.player.animations.play('right');
+        }
+        else
+        {
+            // Stand still
+            this.player.animations.stop();
 
-    //     player.frame = 4;
-    // }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (this.cursors.up.isDown && this.player.body.blocked.down)
-    {
-        this.player.body.velocity.y = -350;
-    }
+            this.player.frame = 3;
+        }
 
+        //  Allow the player to jump if they are touching the ground.
+        if (this.cursors.up.isDown && this.player.body.blocked.down)
+        {
+            this.player.body.velocity.y = -350;
+        }
 
-   // this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
-   // this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null, this);
+        // arcade.collide for barrier Object 
+        // this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
+        // this.game.physics.arcade.overlap(this.player, this.doors, this.enterDoor, null, this);
+    },
 
-   // player movement
-    
-    // this.player.body.velocity.x = 0;
-
-    // if(this.cursors.up.isDown) {
-    //   if(this.player.body.velocity.y == 0)
-    //   this.player.body.velocity.y -= 50;
-    // }
-    // else if(this.cursors.down.isDown) {
-    //   if(this.player.body.velocity.y == 0)
-    //   this.player.body.velocity.y += 50;
-    // }
-    // else {
-    //   this.player.body.velocity.y = 0;
-    // }
-    // if(this.cursors.left.isDown) {
-    //   this.player.body.velocity.x -= 50;
-    // }
-    // else if(this.cursors.right.isDown) {
-    //   this.player.body.velocity.x += 50;
-    // }
-  },
-
-  collect: function(player, collectable) {
-    console.log('yummy!');
-
-    //remove sprite
-    collectable.destroy();
-  },
-  enterDoor: function(player, door) {
-    console.log('entering door that will take you to '+door.targetTilemap+' on x:'+door.targetX+' and y:'+door.targetY);
-  },
+    collect: function(player, collectable) {
+        console.log('collected');
+        // Remove sprite
+        collectable.destroy();
+    },
+    enterDoor: function(player, door) {
+        console.log('entering door that will take you to '+door.targetTilemap+' on x:'+door.targetX+' and y:'+door.targetY);
+    },
 };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var TopDownGame = TopDownGame || {};
 
-TopDownGame.game = new Phaser.Game(1024, 512, Phaser.AUTO, 'window');
+TopDownGame.game = new Phaser.Game(800, 400, Phaser.AUTO, 'window');
 
 TopDownGame.game.state.add('Boot', TopDownGame.Boot);
 TopDownGame.game.state.add('Preload', TopDownGame.Preload);
