64ddbcc1908596262ea30a4e0a2881ff66fbdd77
Jeremy Slavitz <JeremySlavitz@resnet154-41.medford.tufts.edu>
2015-10-18 17:50:06 -0400

    Fixed a few bugs, added toxic liquid

--- js/GameOver.js
+++ js/GameOver.js
@@ -8,17 +8,21 @@ TopDownGame.Gameover.prototype = {
 
 	create: function( ) {
 		this.game.add.tileSprite(0, 0, 800, 400, "spacetitle"); 
-		text = this.game.add.text(100, 100, "GAME OVER. \n press left key to play again");
+		text = this.game.add.text(100, 100, "GAME OVER. \n click or press enter to play again");
         text.font = 'Revalia';
         text.fill = "#00FF00";
 
         var cursors; 
     	this.cursors = this.game.input.keyboard.createCursorKeys();
+    	this.game.input.onTap.addOnce(this.restart,this);
+
 	},
 
 	update: function ( ) {
-		if (this.cursors.left.isDown) {
-    	 		this.state.start('Title'); 
-    	}
+	
 	},
+	restart: function() {
+        this.state.start("Game");
+    },
+
 }; 
