01fcf5ecb761208d8de66b5ff66334947b1bce87
Alena Borisenko <alena.borisenko@gmail.com>
2015-10-19 00:38:48 -0400

    Better use of font globals. Updated GAME OVER screen with the recent font changes in mind

--- js/GameOver.js
+++ js/GameOver.js
@@ -8,9 +8,21 @@ TopDownGame.Gameover.prototype = {
 
 	create: function( ) {
 		this.game.add.tileSprite(0, 0, 800, 400, "spacetitle"); 
-		text = this.game.add.text(100, 100, "GAME OVER. \n click or press enter to play again");
-        text.font = 'Revalia';
-        text.fill = "#00FF00";
+        titleText = this.game.add.text(this.game.width / 2, 100, "GAME OVER!");
+        titleText.anchor.setTo(0.5, 0.5);
+        titleText.font = currFont;
+        titleText.fill = fontColor;
+        titleText.fontSize = titleSize; 
+
+        clickText = this.game.add.text(this.game.width / 2, 330, "Click to play again");
+        clickText.anchor.setTo(0.5, 0.5);
+        clickText.font = currFont;
+        clickText.fill = fontColor;
+        clickText.fontSize = clickSize; 
+
+		//text = this.game.add.text(100, 100, "GAME OVER! \n Click to play again");
+        //text.font = currFont;
+        //text.fill = "#00FF00";
 
         var cursors; 
     	this.cursors = this.game.input.keyboard.createCursorKeys();
